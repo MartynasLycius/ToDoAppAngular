e4d10bf25713f1b2d12dd7929c26435fb5ce6e78
Md. Zohir Raihan <raihanzohir@yahoo.com>
2023-05-21 20:13:08 +0600

    Interface EntityService that used only for naming convention for methods for services

---
+++ todo-service/src/main/java/com/todo/sample/service/EntityService.java
@@ -0,0 +1,22 @@
+package com.todo.sample.service;
+
+import java.util.List;
+
+/**
+ * Created by Md. Zohir Raihan on 5/21/2023.
+ *
+ * Interface that used only for naming convention for methods for services
+ */
+public interface EntityService<E> {
+
+    void create(E e);
+
+    E find(int id);
+
+    E update(E e);
+
+    void delete(int id);
+
+    List<E> find();
+
+}
