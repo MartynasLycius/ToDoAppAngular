e98367912f404854bed74a1b0b927e1a095c44e5
Md. Zohir Raihan <raihanzohir@yahoo.com>
2023-05-21 20:00:27 +0600

    todo-service initial commit with Configure JAX-RS and Filter Responses

---
+++ todo-service/src/main/java/com/todo/sample/config/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.todo.sample.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by Md. Zohir Raihan on 5/21/2023.
+ */
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+}
